02639b150e4f08dc0ccc1bc3a97e251458218426
Quim Granados <quimgranados@gmail.com>
2019-07-08 15:49:16 +1000

    prepare to deploy

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
-const express = require('express');
-var cors = require('cors')
-require('./db/mongoose');
-const userRouter = require('./routers/user');
+const express = require("express");
+const cors = require("cors");
+require("./db/mongoose");
+const path = require("path");
+const userRouter = require("./routers/user");
 
 const app = express();
 
@@ -9,6 +10,16 @@ app.use(express.json());
 
 app.use(cors());
 
-app.use('/users', userRouter);
+app.use("/users", userRouter);
+
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 module.exports = app;
